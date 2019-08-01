3a8136b524aab459645ce33faed750ec142ed8f7
Alexey Burlakov <redflasher@yandex.ru>
2019-08-01 22:15:52 +0300

    hide video when load on ios

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 (function ($) {
     "use strict";
 
-        
-    
+    var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);    
+
+    if(iOS)
+    	$('video').hide();
 
 })(jQuery);
